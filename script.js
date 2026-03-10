f6300204a043789725b07c4932c5a0e3bc32dc9c
NathanFreeman12 <freemannerd@gmail.com>
2026-03-10 17:02:34 -0500

    finished all

--- script.js
+++ script.js
@@ -87,7 +87,24 @@ for (let i = 0; i < friendFavorites.length; i++) {
 
 // 9. Print both arrays and compare:
 // "There are more long-named foods." OR "There are more short-named foods."
+console.log("Long food names:", longFoodNames);
+console.log("Short food names:", shortFoodNames);
+
+if (longFoodNames.length > shortFoodNames.length) {
+    console.log("There are more long-named foods.");
+} else {
+    console.log("There are more short-named foods.");
+}
 
 
 // 10. STRETCH: Find the longest food name and print:
 // "The longest food name in the list is ______ with ___ characters."
+let longestFood = "";
+
+for (let i = 0; i < friendFavorites.length; i++) {
+    if (friendFavorites[i].length > longestFood.length) {
+        longestFood = friendFavorites[i];
+    }
+}
+
+console.log("The longest food name in the list is " + longestFood + " with " + longestFood.length + " characters.");
